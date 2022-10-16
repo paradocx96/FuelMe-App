0af1d5f3e932708985d09b296b8ccfa39286fb0c
Hirush Gimhan <it19240848@my.sliit.lk>
2022-10-17 00:49:33 +0530

    Design add feedback UI

---
+++ app/src/main/java/com/example/fuelme/ui/feedback/AddFeedback.java
@@ -0,0 +1,16 @@
+package com.example.fuelme.ui.feedback;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fuelme.R;
+
+public class AddFeedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_feedback);
+    }
+}
