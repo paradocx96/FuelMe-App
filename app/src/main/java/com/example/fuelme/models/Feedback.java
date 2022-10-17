88d4f56a494e41c4b044de5b965d7d852ee409fe
Hirush Gimhan <it19240848@my.sliit.lk>
2022-10-18 02:46:55 +0530

    Created Feedback model class

---
+++ app/src/main/java/com/example/fuelme/models/Feedback.java
@@ -0,0 +1,72 @@
+package com.example.fuelme.models;
+
+public class Feedback {
+
+    public String id;
+    public String stationId;
+    public String username;
+    public String subject;
+    public String description;
+    public String createAt;
+
+
+    public Feedback() {
+    }
+
+    public Feedback(String id, String stationId, String username, String subject, String description, String createAt) {
+        this.id = id;
+        this.stationId = stationId;
+        this.username = username;
+        this.subject = subject;
+        this.description = description;
+        this.createAt = createAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(String stationId) {
+        this.stationId = stationId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+}
