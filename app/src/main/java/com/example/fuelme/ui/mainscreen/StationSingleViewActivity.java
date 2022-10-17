4218ed049091b95c010172180cce20253e6c569b
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-17 20:56:05 +0530

    Added StationSingleViewActivity.java and its layout

---
+++ app/src/main/java/com/example/fuelme/ui/mainscreen/StationSingleViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.fuelme.ui.mainscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fuelme.R;
+
+public class StationSingleViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_station_single_view);
+    }
+}
