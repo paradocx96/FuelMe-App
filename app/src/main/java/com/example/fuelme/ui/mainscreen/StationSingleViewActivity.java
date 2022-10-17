81027a598467b18b55b02a6bb367071690a1fee7
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-17 23:46:41 +0530

    Edited station single view layout file

--- app/src/main/java/com/example/fuelme/ui/mainscreen/StationSingleViewActivity.java
+++ app/src/main/java/com/example/fuelme/ui/mainscreen/StationSingleViewActivity.java
@@ -1,6 +1,7 @@
 package com.example.fuelme.ui.mainscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -12,5 +13,24 @@ public class StationSingleViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_single_view);
+
+        //instantiate toolbar and set the back button
+        Toolbar toolbar = (Toolbar) findViewById(R.id.station_single_view_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    //method called when toolbar back button is clicked
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    //handle back press
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
