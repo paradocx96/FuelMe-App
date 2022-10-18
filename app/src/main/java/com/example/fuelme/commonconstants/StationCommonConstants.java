21188f3806ca1cdf2dcc0c86904b2f4406d18bb4
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-18 23:28:08 +0530

    Added StationCommonConstants.java

---
+++ app/src/main/java/com/example/fuelme/commonconstants/StationCommonConstants.java
@@ -0,0 +1,6 @@
+package com.example.fuelme.commonconstants;
+
+public class StationCommonConstants {
+    public static final String REMOTE_URL_FUEL_STATION = "https://fuelme.azurewebsites.net/";
+    public static final String STATION_SHARED_PREF_NAME = "fuel_station_shared_pref";
+}
