6740f6312c23de6fd758cb4e890b60f13bb5702c
Navinda Chandrasiri <adilanga@gmail.com>
2022-10-18 06:20:52 +0530

    Notice List Station Owner UI Impl

---
+++ app/src/main/java/com/example/fuelme/ui/notice/NoticeListStationActivity.java
@@ -0,0 +1,16 @@
+package com.example.fuelme.ui.notice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fuelme.R;
+
+public class NoticeListStationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_list_station);
+    }
+}
