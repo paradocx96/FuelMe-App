f452bcd3e2ead6c6550b7cb551f7f52f5392ac32
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-18 12:35:25 +0530

    Created the form layout of the activity_register_station.xml

--- app/src/main/java/com/example/fuelme/ui/owner_dashboard_screen/OwnerDashboardActivity.java
+++ app/src/main/java/com/example/fuelme/ui/owner_dashboard_screen/OwnerDashboardActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.fuelme.R;
+import com.example.fuelme.ui.register_station_screen.RegisterStationActivity;
 
 public class OwnerDashboardActivity extends AppCompatActivity {
 
@@ -32,7 +33,8 @@ public class OwnerDashboardActivity extends AppCompatActivity {
     }
 
     public void navigateToRegisterStation(View view){
-        //Intent intent = new Intent(this, )
+        Intent intent = new Intent(this, RegisterStationActivity.class);
+        startActivity(intent);
     }
 
     public void  navigateToUpdate(View view){
