fe7ecea936ffe83a4cf5c52bc6d1c6b25cb2fd57
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-19 11:04:26 +0530

    Added shared preferences to StationCommonConstants.java

--- app/src/main/java/com/example/fuelme/commonconstants/StationCommonConstants.java
+++ app/src/main/java/com/example/fuelme/commonconstants/StationCommonConstants.java
@@ -3,4 +3,6 @@ package com.example.fuelme.commonconstants;
 public class StationCommonConstants {
     public static final String REMOTE_URL_FUEL_STATION = "https://fuelme.azurewebsites.net/";
     public static final String STATION_SHARED_PREF_NAME = "fuel_station_shared_pref";
+    public static final String IN_QUEUE_STATION_ID = "station_id";
+    public static final String QUEUE = "queue";
 }
