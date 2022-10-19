f379662311beabfb92923cc9338cfbec23b2d7a6
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-19 13:50:52 +0530

    Added ids to text views in activity_register_station.xml

--- app/src/main/java/com/example/fuelme/ui/register_station_screen/RegisterStationActivity.java
+++ app/src/main/java/com/example/fuelme/ui/register_station_screen/RegisterStationActivity.java
@@ -4,10 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.fuelme.R;
 
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+
+/*
+IT19014128
+A.M.W.W.R.L. Wataketiya
+Activity for registering a new station
+*/
+
 public class RegisterStationActivity extends AppCompatActivity {
+
+    private final OkHttpClient client = new OkHttpClient(); //okhttp client instance
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+    final String TAG = "demo";//debug tag
+
+    EditText editTextLicense, editTextStationName, editTextStationAddress, editTextStationEmail, editTextStationWebsite;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +39,19 @@ public class RegisterStationActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //register the views
+        editTextLicense = findViewById(R.id.edtText_license_register_station);
+        editTextStationName = findViewById(R.id.edtText_stationName_register_station);
+        editTextStationAddress = findViewById(R.id.edtText_stationAddress_register_station);
+        editTextStationEmail = findViewById(R.id.edtText_stationEmail_register_station);
+        editTextStationWebsite = findViewById(R.id.edtText_stationWebsite_register_station);
+
+    }
+
+    //button click for register station button
+    public void registerStationButtonClick(View view){
+
     }
 
 
