aa9b1ab95f4981bded3c08800f94c28655d8c2b9
Navinda Lankesh <adilanga@gmail.com>
2022-10-20 22:55:20 +0530

    Notice Station List API Config Updated

--- app/src/main/java/com/example/fuelme/ui/notice/NoticeListStationActivity.java
+++ app/src/main/java/com/example/fuelme/ui/notice/NoticeListStationActivity.java
@@ -4,11 +4,15 @@ import static com.example.fuelme.commonconstants.CommonConstants.REMOTE_URL_NOTI
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.fuelme.R;
 import com.example.fuelme.models.notice.Notice;
@@ -30,28 +34,57 @@ import okhttp3.ResponseBody;
 
 public class NoticeListStationActivity extends AppCompatActivity {
 
-    ArrayList<Notice> noticeArrayList;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    String logged_username, station_id;
+
     RecyclerView recyclerViewNotice;
+    NoticeListStationAdapter adapter;
+
+    ArrayList<Notice> noticeArrayList;
     private final OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Defined Layout objects of this activity
         setContentView(R.layout.activity_notice_list_station);
         recyclerViewNotice = findViewById(R.id.notice_list_station);
+
+        // Get logged user's data
+        preferences = getSharedPreferences("login_data", MODE_PRIVATE);
+        editor = preferences.edit();
+        logged_username = preferences.getString("user_username", "");
+
+        // Defined intent data
+        Intent intent = getIntent();
+        station_id = intent.getStringExtra("station_id");
+        Log.d("API_CALL", "STATION_ID => " + station_id);
+
+        // Defined toolbar and set the back button
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_notice_list_station);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         noticeArrayList = new ArrayList<>();
+
+        adapter = new NoticeListStationAdapter(getApplicationContext(), noticeArrayList);
+        recyclerViewNotice.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
         getNotices();
     }
 
     private void getNotices() {
         Request request = new Request.Builder()
-                .url(REMOTE_URL_NOTICE_STATION + "002")
+                .url(REMOTE_URL_NOTICE_STATION + station_id)
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Toast.makeText(NoticeListStationActivity.this, "No Notice", Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
 
@@ -62,40 +95,45 @@ public class NoticeListStationActivity extends AppCompatActivity {
                         throw new IOException("Unexpected code " + response);
                     String body = responseBody.string();
 
-                    Log.d("API_CALL", "JSON RESPONSE => " + body);
-
                     JSONArray jsonArr = new JSONArray(body);
-                    Notice notice = new Notice();
+                    Notice notice;
 
                     for (int i = 0; i < jsonArr.length(); i++) {
                         JSONObject object = jsonArr.getJSONObject(i);
-
-                        notice.setId(object.getString("id"));
-                        notice.setStationId(object.getString("stationId"));
-                        notice.setTitle(object.getString("title"));
-                        notice.setDescription(object.getString("description"));
-                        notice.setAuthor(object.getString("author"));
-                        notice.setCreateAt(object.getString("createAt"));
+                        notice = new Notice(
+                                object.getString("id"),
+                                object.getString("stationId"),
+                                object.getString("title"),
+                                object.getString("description"),
+                                object.getString("author"),
+                                object.getString("createAt")
+                        );
 
                         noticeArrayList.add(notice);
 
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                NoticeListStationAdapter adapter = new NoticeListStationAdapter(getApplicationContext(), noticeArrayList);
-                                recyclerViewNotice.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
                                 recyclerViewNotice.setAdapter(adapter);
                             }
                         });
                     }
-
-                    Log.d("API_CALL", "Array Size onResponse: " + noticeArrayList.size());
                 } catch (JSONException | NullPointerException e) {
                     e.printStackTrace();
                 }
             }
         });
+    }
 
-        Log.d("API_CALL", "Array Size getNotices: " + noticeArrayList.size());
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/example/fuelme/ui/notice/adapter/NoticeListStationAdapter.java
+++ app/src/main/java/com/example/fuelme/ui/notice/adapter/NoticeListStationAdapter.java
@@ -1,16 +1,21 @@
 package com.example.fuelme.ui.notice.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fuelme.R;
 import com.example.fuelme.models.notice.Notice;
+import com.example.fuelme.ui.notice.NoticeViewStationActivity;
 
 import java.util.ArrayList;
 
@@ -27,19 +32,32 @@ public class NoticeListStationAdapter extends RecyclerView.Adapter<NoticeListSta
     @NonNull
     @Override
     public NoticeListStationAdapter.NoticeListStationHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view;
         LayoutInflater inflater = LayoutInflater.from(context);
-        view = inflater.inflate(R.layout.notice_list_item_station, parent, false);
+        View view = inflater.inflate(R.layout.notice_list_item_station, parent, false);
 
         return new NoticeListStationAdapter.NoticeListStationHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NoticeListStationAdapter.NoticeListStationHolder holder, int position) {
-        holder.title.setText(noticeArrayList.get(position).getTitle());
-        holder.description.setText(noticeArrayList.get(position).getDescription());
-        holder.author.setText(noticeArrayList.get(position).getAuthor());
-        holder.createdAt.setText(noticeArrayList.get(position).getCreateAt());
+    public void onBindViewHolder(@NonNull NoticeListStationAdapter.NoticeListStationHolder holder, @SuppressLint("RecyclerView") int position) {
+
+        Notice notice = noticeArrayList.get(position);
+        holder.setCardViewData(notice);
+
+        holder.notice_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, NoticeViewStationActivity.class);
+                intent.putExtra("notice_id", noticeArrayList.get(position).getId());
+                intent.putExtra("notice_station_id", noticeArrayList.get(position).getStationId());
+                intent.putExtra("notice_title", noticeArrayList.get(position).getTitle());
+                intent.putExtra("notice_description", noticeArrayList.get(position).getDescription());
+                intent.putExtra("notice_author", noticeArrayList.get(position).getAuthor());
+                intent.putExtra("notice_created", noticeArrayList.get(position).getCreateAt());
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -48,7 +66,9 @@ public class NoticeListStationAdapter extends RecyclerView.Adapter<NoticeListSta
     }
 
     public static class NoticeListStationHolder extends RecyclerView.ViewHolder {
-        TextView id, title, description, author, stationId, createdAt;
+        TextView title, description, author, createdAt;
+        ConstraintLayout notice_item;
+        LinearLayout notice_item_linear;
 
         public NoticeListStationHolder(@NonNull View itemView) {
             super(itemView);
@@ -56,6 +76,15 @@ public class NoticeListStationAdapter extends RecyclerView.Adapter<NoticeListSta
             description = itemView.findViewById(R.id.notice_item_description3);
             author = itemView.findViewById(R.id.notice_item_author3);
             createdAt = itemView.findViewById(R.id.notice_item_created3);
+            notice_item = itemView.findViewById(R.id.notice_item_station);
+            notice_item_linear = itemView.findViewById(R.id.notice_list_item_sta);
+        }
+
+        void setCardViewData(Notice notice) {
+            title.setText(notice.getTitle());
+            description.setText(notice.getDescription());
+            author.setText(notice.getAuthor());
+            createdAt.setText(notice.getCreateAt());
         }
     }
 }
