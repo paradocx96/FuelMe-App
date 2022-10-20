139cfac6125dae79305c9357703fc7ec19143c1e
Navinda Lankesh <adilanga@gmail.com>
2022-10-20 22:52:25 +0530

    Notice Constants Updated

--- app/src/main/java/com/example/fuelme/commonconstants/CommonConstants.java
+++ app/src/main/java/com/example/fuelme/commonconstants/CommonConstants.java
@@ -5,6 +5,7 @@ public class CommonConstants {
     public static final String REMOTE_URL_AUTH = REMOTE_URL + "api/Auth/";
     public static final String REMOTE_URL_AUTH_LOGIN = REMOTE_URL + "api/Auth/login";
     public static final String REMOTE_URL_AUTH_REGISTER = REMOTE_URL + "api/Auth/register";
-    public static final String REMOTE_URL_NOTICE = REMOTE_URL + "api/Notice";
+    public static final String REMOTE_URL_NOTICE = REMOTE_URL + "api/Notice/";
     public static final String REMOTE_URL_NOTICE_STATION = REMOTE_URL + "api/Notice/station/";
+    public static final String REMOTE_URL_NOTICE_AUTHOR = REMOTE_URL + "api/Notice/author/";
 }
