27354c7b17e16c72953ed7ff9c94bf33979d9a85
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-20 14:05:27 +0530

    Added logic to fetch fuel stations from remote to AllStationsFragment.java

--- app/src/main/java/com/example/fuelme/ui/mainscreen/fragments/AllStationsFragment.java
+++ app/src/main/java/com/example/fuelme/ui/mainscreen/fragments/AllStationsFragment.java
@@ -2,19 +2,36 @@ package com.example.fuelme.ui.mainscreen.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.fuelme.R;
+import com.example.fuelme.commonconstants.CommonConstants;
 import com.example.fuelme.models.FuelStation;
 import com.example.fuelme.ui.mainscreen.adapters.AllStationsRecyclerViewAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +42,9 @@ public class AllStationsFragment extends Fragment {
 
     private final String TAG = "demo";
     ArrayList<FuelStation> fuelStations = new ArrayList<>(); //array list for fuel stations
+    private final OkHttpClient client = new OkHttpClient(); //okhttp client instance
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -73,7 +93,10 @@ public class AllStationsFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_all_stations, container, false);
 
         //setup the fuel station list
-        setupFuelStations();
+        //setupSampleFuelStations();
+
+        //fetch and assign the fuel station list
+        fetchFuelStations();
 
         //assign recycler view
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView_allStations);
@@ -86,8 +109,86 @@ public class AllStationsFragment extends Fragment {
         return view;
     }
 
+    //fetch all the stations from remote
+    public void fetchFuelStations(){
+        String baseUrl = CommonConstants.REMOTE_URL;
+
+        //build the url using Url builder
+        HttpUrl url = HttpUrl.parse(baseUrl).newBuilder()
+                .addPathSegment("api")
+                .addPathSegment("FuelStations")
+                .build();
+
+        //build the request
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        //make the client call using okhttp
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d(TAG, "Failed to make call");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()){
+                    //handle successful response
+                    ResponseBody responseBody = response.body();
+                    String body = responseBody.string();
+
+                    //get the body to a JSON object
+                    try {
+                        JSONArray jsonArray = new JSONArray(body); //get the json array
+                        //iterate through the JSON array
+                        for (int i = 0; i < jsonArray.length(); i++){
+                            JSONObject jsonObject = new JSONObject(body);
+                            jsonObject = jsonArray.getJSONObject(i); //get the json object by index
+
+                            FuelStation fuelStation = new FuelStation();//instantiate fuel station object
+
+                            //assign attributes to the fuel station object
+                            fuelStation.setId(jsonObject.getString("id"));
+                            fuelStation.setLicense(jsonObject.getString("license"));
+                            fuelStation.setOwnerUsername(jsonObject.getString("ownerUsername"));
+                            fuelStation.setStationName(jsonObject.getString("stationName"));
+                            fuelStation.setStationAddress(jsonObject.getString("stationAddress"));
+                            fuelStation.setStationPhoneNumber(jsonObject.getString("stationPhoneNumber"));
+                            fuelStation.setStationEmail(jsonObject.getString("stationEmail"));
+                            fuelStation.setStationWebsite(jsonObject.getString("stationWebsite"));
+                            fuelStation.setOpenStatus(jsonObject.getString("openStatus"));
+                            fuelStation.setPetrolQueueLength(jsonObject.getInt("petrolQueueLength"));
+                            fuelStation.setDieselQueueLength(jsonObject.getInt("dieselQueueLength"));
+                            fuelStation.setPetrolStatus(jsonObject.getString("petrolStatus"));
+                            fuelStation.setDieselStatus(jsonObject.getString("dieselStatus"));
+                            fuelStation.setLocationLatitude(jsonObject.getInt("locationLatitude"));
+                            fuelStation.setLocationLongitude(jsonObject.getInt("locationLongitude"));
+
+                            //add the fuel station to fuel stations list
+                            fuelStations.add(fuelStation);
+                        }
+
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    //handle unsuccessful response
+                    ResponseBody responseBody = response.body();
+                    String body = responseBody.string();
+                    Log.d(TAG, "onResponse failure : " + body);
+                }
+            }
+        });
+
+        Log.d(TAG, "URL : " +url);
+
+    }
+
     //method for fetching data and assigning to fuel stations array list
-    public void setupFuelStations(){
+    public void setupSampleFuelStations(){
         FuelStation fuelStation1 = new FuelStation("0001","l001" ,"eheliyagoda",
                 "Eheliyagoda Assotiates","41/8, Sangabo Mawatha, Colombo", "01144552",
                 "ehe@gmail.com","ehe.com","open",
