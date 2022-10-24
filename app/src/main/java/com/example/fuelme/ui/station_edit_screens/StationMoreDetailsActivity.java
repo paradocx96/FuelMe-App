2b4694402dc04712c34fec9b441c54ba72b6601b
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-24 11:39:46 +0530

    Set toolbar in StationMoreDetailsActivity

--- app/src/main/java/com/example/fuelme/ui/station_edit_screens/StationMoreDetailsActivity.java
+++ app/src/main/java/com/example/fuelme/ui/station_edit_screens/StationMoreDetailsActivity.java
@@ -1,8 +1,10 @@
 package com.example.fuelme.ui.station_edit_screens;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
 
 import com.example.fuelme.R;
 
@@ -12,5 +14,21 @@ public class StationMoreDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_station_more_details);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.station_more_details_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
