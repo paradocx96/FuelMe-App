82700f7da021578a3b27578953de89cc01a1ac95
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-24 11:19:22 +0530

    Added StationMoreDetailsActivity and its layout

---
+++ app/src/main/java/com/example/fuelme/ui/station_edit_screens/StationMoreDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.fuelme.ui.station_edit_screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fuelme.R;
+
+public class StationMoreDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_station_more_details);
+    }
+}
