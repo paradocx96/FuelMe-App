40529bc4cec2d1123d580180922249ff64af929b
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-24 23:23:20 +0530

    Updated CustomerRefuelHistoryRecyclerViewAdapter.java

--- app/src/main/java/com/example/fuelme/ui/customer_dashboard/adapters/CustomerRefuelHistoryRecyclerViewAdapter.java
+++ app/src/main/java/com/example/fuelme/ui/customer_dashboard/adapters/CustomerRefuelHistoryRecyclerViewAdapter.java
@@ -11,9 +11,17 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fuelme.R;
+import com.example.fuelme.helpers.DateTimeHelper;
 import com.example.fuelme.models.QueueLogItem;
+import com.example.fuelme.models.time.CustomDateTime;
 
 import java.util.ArrayList;
+
+/*
+* IT19014128
+* A.M.W.W.R.L. Wataketiya
+* Adapter for customer history recycler view row
+* */
 
 public class CustomerRefuelHistoryRecyclerViewAdapter extends RecyclerView.Adapter<CustomerRefuelHistoryRecyclerViewAdapter.MyViewHolder> {
 
@@ -39,6 +47,66 @@ public class CustomerRefuelHistoryRecyclerViewAdapter extends RecyclerView.Adapt
 
     @Override
     public void onBindViewHolder(@NonNull CustomerRefuelHistoryRecyclerViewAdapter.MyViewHolder holder, int position) {
+
+        //get the queue log item for the position
+        QueueLogItem queueLogItem = queueLogItems.get(position);
+
+        //get the date and time integers
+        int year = queueLogItem.getYear();
+        int month = queueLogItem.getMonth();
+        int dayNumber = queueLogItem.getDayNumber();
+        int hour = queueLogItem.getHour();
+        int minute = queueLogItem.getMinute();
+        int second = queueLogItem.getSecond();
+
+        //instantiate custom date time
+        CustomDateTime customDateTimeUTC = new  CustomDateTime(year, month,dayNumber, hour, minute, second);
+        //get the time converted Sri Lankan time zone
+        CustomDateTime customDateTimeSL = DateTimeHelper.convertUTCToSLTime(customDateTimeUTC);
+
+        //get the date and time into strings
+        String dateString = DateTimeHelper.getDateInISOFormat(customDateTimeSL);
+        String timeString = DateTimeHelper.getTimeInTwentyFourHourFormat(customDateTimeSL);
+
+        //get the queue, action and refuel status
+        String queueString = queueLogItem.getQueue();
+        String actionString = queueLogItem.getAction();
+        String refuelStatusString = queueLogItem.getRefuelStatus();
+
+
+        //change strings to better support capitalization
+        if (queueString.equalsIgnoreCase("petrol")){
+            queueString = "Petrol";
+        }
+        else if (queueString.equalsIgnoreCase("diesel")){
+            queueString = "Diesel";
+        }
+
+        if (actionString.equalsIgnoreCase("join")){
+            actionString = "Join";
+        }
+        else if (actionString.equalsIgnoreCase("leave")){
+            actionString = "Leave";
+        }
+
+        if (refuelStatusString.equalsIgnoreCase("refueled")){
+            refuelStatusString = "Refueled";
+        }
+        else if (refuelStatusString.equalsIgnoreCase("not-refueled")){
+            refuelStatusString = "Not Refueled";
+        }
+        else if (refuelStatusString.equalsIgnoreCase("not-appliable")){
+            refuelStatusString = "Not Applicable";
+        }
+
+        //assigning values to the text views
+        holder.textViewDate.setText(dateString);
+        holder.textViewTime.setText(timeString);
+        holder.textViewStationId.setText(queueLogItem.getStationId());
+        holder.textViewStationName.setText(queueLogItem.getStationName());
+        holder.textViewQueue.setText(queueString);
+        holder.textViewAction.setText(actionString);
+        holder.textViewRefuelStatus.setText(refuelStatusString);
 
     }
 
