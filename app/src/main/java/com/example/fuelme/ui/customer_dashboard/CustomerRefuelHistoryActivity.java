29688ffc9568be63d17a7093770b43b08d259ab5
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-10-24 21:22:19 +0530

    Added CustomerRefuelHistoryActivity and its layout

---
+++ app/src/main/java/com/example/fuelme/ui/customer_dashboard/CustomerRefuelHistoryActivity.java
@@ -0,0 +1,17 @@
+package com.example.fuelme.ui.customer_dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fuelme.R;
+
+
+public class CustomerRefuelHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_refuel_history);
+    }
+}
