b069f2e9a8a00fd0c040baff5c1b5db1a51f259a
Navinda Lankesh <adilanga@gmail.com>
2022-10-25 16:08:20 +0530

    Comments added to Notice List Station Activity

--- app/src/main/java/com/example/fuelme/ui/notice/NoticeListStationActivity.java
+++ app/src/main/java/com/example/fuelme/ui/notice/NoticeListStationActivity.java
@@ -1,3 +1,11 @@
+/**
+ * FuelMe APP
+ * Enterprise Application Development - SE4040
+ *
+ * @author IT19180526 - S.A.N.L.D. Chandrasiri
+ * @version 1.0
+ */
+
 package com.example.fuelme.ui.notice;
 
 import static com.example.fuelme.commonconstants.CommonConstants.REMOTE_URL_NOTICE_STATION;
@@ -12,7 +20,6 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.example.fuelme.R;
@@ -33,19 +40,32 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
+/**
+ * Notice List View for Station Owner UI for FuelMe Application
+ *
+ * @author IT19180526 - S.A.N.L.D. Chandrasiri
+ * @version 1.0
+ */
 public class NoticeListStationActivity extends AppCompatActivity {
 
+    // Defined object and variables
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
     String logged_username, station_id;
-
     RecyclerView recyclerViewNotice;
     SwipeRefreshLayout swipeRefreshLayout;
     NoticeListStationAdapter adapter;
-
     ArrayList<Notice> noticeArrayList;
     private final OkHttpClient client = new OkHttpClient();
 
+    /**
+     * This method used for bind the layout UI element with defined local object.
+     * Also defined onclick refresh listener for swipe refresh.
+     * Then call getNotices() method to get notice data from API.
+     *
+     * @param savedInstanceState - Bundle
+     * @see #onCreate(Bundle savedInstanceState)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,13 +90,15 @@ public class NoticeListStationActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        // Defined and initialize object need for view List view
         noticeArrayList = new ArrayList<>();
-
         adapter = new NoticeListStationAdapter(getApplicationContext(), noticeArrayList);
         recyclerViewNotice.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
+        // Calling notice retrieve method for get notice from database
         getNotices();
 
+        // Initialize swipe refresh list view
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -87,30 +109,52 @@ public class NoticeListStationActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * This method used for getting notice data from database and view in list view
+     * Execute HTTP request to get notice from database
+     *
+     * @throws IOException          - Handle the Exception produced by failed or interrupted I/O operations.
+     * @throws JSONException        - Handle the Exception produced by JSON manipulation operations.
+     * @throws NullPointerException - Handle the Exception produced by null values operating.
+     * @see #getNotices()
+     */
     private void getNotices() {
+        // Create HTTP request for get notice by station id
         Request request = new Request.Builder()
                 .url(REMOTE_URL_NOTICE_STATION + station_id)
                 .build();
 
+        // Execute HTTP request and check whether request response
         client.newCall(request).enqueue(new Callback() {
+            // HTTP Request when failed this will execute.
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 Toast.makeText(NoticeListStationActivity.this, "No Notice", Toast.LENGTH_SHORT).show();
-                e.printStackTrace();
             }
 
+            // HTTP Request when response got from API this will execute.
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                // Define JSON object using API response
                 try (ResponseBody responseBody = response.body()) {
+                    // Check response is success or not
                     if (!response.isSuccessful())
                         throw new IOException("Unexpected code " + response);
+
+                    // Define string by response body
                     String body = responseBody.string();
 
+                    // Define JSON array using string response body
                     JSONArray jsonArr = new JSONArray(body);
+
+                    // Define notice object
                     Notice notice;
 
                     for (int i = 0; i < jsonArr.length(); i++) {
+                        // Create JSON object using JSON array object
                         JSONObject object = jsonArr.getJSONObject(i);
+
+                        // Create notice object with database data
                         notice = new Notice(
                                 object.getString("id"),
                                 object.getString("stationId"),
@@ -120,8 +164,10 @@ public class NoticeListStationActivity extends AppCompatActivity {
                                 object.getString("createAt")
                         );
 
+                        // Add notice data to notice arraylist
                         noticeArrayList.add(notice);
 
+                        // Call List view adapter to view the list view
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
@@ -136,13 +182,22 @@ public class NoticeListStationActivity extends AppCompatActivity {
         });
     }
 
-
+    /**
+     * This method used for handle the toolbar
+     *
+     * @see #onBackPressed()
+     */
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
     }
 
+    /**
+     * This method used for handle the toolbar
+     *
+     * @see #onBackPressed()
+     */
     @Override
     public void onBackPressed() {
         super.onBackPressed();
